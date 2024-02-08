bf2955f94f9df1bbb07e474cf9affcc885270033
Tolga Yildirim <tolgayld1990@gmail.com>
2024-02-08 01:40:57 +0100

    Return types changed from ResultFuture<Type> to dynamic

--- lib/core/usecases/usecases.dart
+++ lib/core/usecases/usecases.dart
@@ -1,13 +1,11 @@
-import 'package:locall_app/core/utils/typedefs.dart';
-
 abstract class UsecaseWithParams<Type, Params> {
   const UsecaseWithParams();
 
-  ResultFuture<Type> call(Params params);
+  dynamic call(Params params);
 }
 
 abstract class UsecaseWithoutParams<Type> {
   const UsecaseWithoutParams();
 
-  ResultFuture<Type> call();
+  dynamic call();
 }
