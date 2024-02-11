474b80e24b9dd07b9f49072471675edd0c311c3a
Tolga Yildirim <tolgayld1990@gmail.com>
2024-02-11 14:44:40 +0100

    improve path structure

---
+++ lib/core/common/constants.dart
@@ -0,0 +1,5 @@
+const kDefaultAvatar = 'https://images.freeimages'
+    '.com/fic/images/icons/573/must_have/256/user.png';
+const kCachedThemeMode = 'CACHED_THEME_MODE';
+
+const kCachedUseSystemTheme = 'CHACHED_USE_SYSTEM_THEME';
