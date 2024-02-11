1923150e69002d9e444ccfec1b9f1b8ba6904537
Tolga Yildirim <tolgayld1990@gmail.com>
2024-02-11 02:55:37 +0100

    code improvements

--- lib/core/services/dependency_injection/theme_mode/theme_mode.dart
+++ lib/core/services/dependency_injection/theme_mode/theme_mode.dart
@@ -1,7 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:locall_app/src/application/theme_mode/theme_mode.dart';
-import 'package:locall_app/src/data/datasources/theme_mode_local_datasrc.dart';
-import 'package:locall_app/src/data/repositories/theme_mode_repo_impl.dart';
+import 'package:locall_app/src/data/datasources/theme_mode/theme_mode_local_datasrc.dart';
+import 'package:locall_app/src/data/repositories/theme_mode/theme_mode_repo_impl.dart';
 import 'package:locall_app/src/domain/repositories/theme_mode/theme_mode_repo.dart';
 import 'package:locall_app/src/domain/usecases/theme_mode/get_theme_mode.dart';
 import 'package:locall_app/src/domain/usecases/theme_mode/get_use_system_theme.dart';
