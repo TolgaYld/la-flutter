48df61a4e96a27561ae6313f2a569c5a53d82718
Tolga Yildirim <tolgayld1990@gmail.com>
2024-02-12 13:29:48 +0100

    fix

--- lib/core/common/env/environment.dart
+++ lib/core/common/env/environment.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Enviroment {
+  Enviroment._();
   static String get fileName {
     if (kReleaseMode) {
       return '.env.production';
