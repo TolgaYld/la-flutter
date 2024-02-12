957538b1ac77f4c55e7fd4aed9c716c53b148151
Tolga Yildirim <tolgayld1990@gmail.com>
2024-02-12 18:45:45 +0100

    String email property changed to emailOrUsername

--- lib/src/data/datasources/auth/auth_remote_datasrc.dart
+++ lib/src/data/datasources/auth/auth_remote_datasrc.dart
@@ -10,7 +10,8 @@ abstract class AuthRemoteDatasrc {
 
   Future<void> forgotPassword(String email);
 
-  Future<UserModel> signIn({required String email, required String password});
+  Future<UserModel> signIn(
+      {required String emailOrUsername, required String password});
 
   Future<UserModel> signUp({
     required String username,
@@ -26,4 +27,55 @@ abstract class AuthRemoteDatasrc {
   });
 
   Future<void> updateUser(UserModel user);
+}
+
+class AuthRemoteDatasrcImpl implements AuthRemoteDatasrc {
+  @override
+  Future<UserModel> authWithProvider({
+    required AuthWithProvider provider,
+    required String providerId,
+    required String email,
+  }) {
+    // TODO: implement authWithProvider
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> forgotPassword(String email) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<UserModel> signIn(
+      {required String emailOrUsername, required String password}) {
+    // TODO: implement signIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<UserModel> signUp({
+    required String username,
+    required String email,
+    required String password,
+    required String repeatPassword,
+    required List<double> coordinates,
+  }) {
+    // TODO: implement signUp
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> updatePassword({
+    required String password,
+    required String repeatPassword,
+  }) {
+    // TODO: implement updatePassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> updateUser(UserModel user) {
+    // TODO: implement updateUser
+    throw UnimplementedError();
+  }
 }
