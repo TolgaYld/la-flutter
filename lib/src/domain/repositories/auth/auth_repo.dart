0f05a24288974ae7c227424966853247f32c2a39
Tolga Yildirim <tolgayld1990@gmail.com>
2024-02-12 04:11:40 +0100

    linting

--- lib/src/domain/repositories/auth/auth_repo.dart
+++ lib/src/domain/repositories/auth/auth_repo.dart
@@ -5,12 +5,13 @@ import 'package:locall_app/src/domain/entities/user.dart';
 abstract class AuthRepo {
   const AuthRepo();
 
-  ResultFuture<User> signUp(
-      {required String username,
-      required String email,
-      required String password,
-      required String repeatPassword,
-      required List<double> coordinates});
+  ResultFuture<User> signUp({
+    required String username,
+    required String email,
+    required String password,
+    required String repeatPassword,
+    required List<double> coordinates,
+  });
 
   ResultFuture<User> authWithProvider({
     required AuthWithProvider provider,
