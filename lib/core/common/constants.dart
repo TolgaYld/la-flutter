a2702b28d520823de8f0e92f3857a14aae3e9dfa
Tolga Yildirim <tolgayld1990@gmail.com>
2024-02-14 03:07:42 +0100

    token & refreshToken keys added

--- lib/core/common/constants.dart
+++ lib/core/common/constants.dart
@@ -1,5 +1,10 @@
 const kDefaultAvatar = 'https://images.freeimages'
     '.com/fic/images/icons/573/must_have/256/user.png';
-const kCachedThemeMode = 'CACHED_THEME_MODE';
 
-const kCachedUseSystemTheme = 'CHACHED_USE_SYSTEM_THEME';
+const kCachedThemeModeKey = 'CACHED_THEME_MODE';
+
+const kCachedUseSystemThemeKey = 'CHACHED_USE_SYSTEM_THEME';
+
+const kCachedTokenKey = 'TOKEN';
+
+const kCachedRefreshTokenKey = 'REFRESHTOKEN';
