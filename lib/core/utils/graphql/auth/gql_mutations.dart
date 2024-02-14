ef98ba1403144a3bfe472af96aa4a1938537b601
Tolga Yildirim <tolgayld1990@gmail.com>
2024-02-14 03:07:57 +0100

    signup mutation

--- lib/core/utils/graphql/auth/gql_mutations.dart
+++ lib/core/utils/graphql/auth/gql_mutations.dart
@@ -15,10 +15,46 @@ mutation SignUpUser(\$data: SignUpUserInput!) {
       is_deleted
       is_banned
       is_admin
+      provider
+      provider_id
+      device_id
+      device_type
       auto_generated_username
       username_change_counter
       created_at
     }
+    token
+    refreshToken
+  }
+}
+''';
+
+  static String signInMutation = '''
+mutation SignInUser(\$data: SignInUserInput!) {
+  signInUser(data: \$data) {
+    user {
+      _id
+      username
+      email
+      location {
+        coordinates
+      }
+      distance
+      points
+      official
+      is_deleted
+      is_banned
+      is_admin
+      provider
+      provider_id
+      device_id
+      device_type
+      auto_generated_username
+      username_change_counter
+      created_at
+    }
+    token
+    refreshToken
   }
 }
 ''';
