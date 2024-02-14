ca3b71f24b96ecafba0cfe211070092a7ef29e59
Tolga Yildirim <tolgayld1990@gmail.com>
2024-02-14 15:14:56 +0100

    new mutations added

--- lib/core/utils/graphql/auth/gql_mutations.dart
+++ lib/core/utils/graphql/auth/gql_mutations.dart
@@ -58,4 +58,44 @@ mutation SignInUser(\$data: SignInUserInput!) {
   }
 }
 ''';
+
+  static String authWithProviderMutation = '''
+mutation AuthUserWithProvider(\$data: AuthUserWithProviderInput) {
+  authUserWithProvider(data: \$data) {
+    user {
+      _id
+      username
+      email
+      location {
+        coordinates
+      }
+      distance
+      points
+      official
+      is_deleted
+      is_banned
+      is_admin
+      provider
+      provider_id
+      device_id
+      device_type
+      auto_generated_username
+      username_change_counter
+      created_at
+    }
+    tokens {
+      token
+      refreshToken
+    }
+  }
+}
+''';
+
+  static String forgotPasswordMutation = '''
+mutation ResetPassword(\$email: String) {
+  resetPassword(email: \$email) {
+    email,
+  }
+}
+''';
 }
