744a07bce4c5f858ccfa6c5975f7d2c68832f6e5
Tolga Yildirim <tolgayld1990@gmail.com>
2024-02-16 21:37:11 +0100

    new core widget

---
+++ lib/core/common/widgets/custom_text_form_field_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormFieldWidget extends StatefulWidget {
+  const CustomTextFormFieldWidget({super.key});
+
+  @override
+  State<CustomTextFormFieldWidget> createState() =>
+      _CustomTextFormFieldWidgetState();
+}
+
+class _CustomTextFormFieldWidgetState extends State<CustomTextFormFieldWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
