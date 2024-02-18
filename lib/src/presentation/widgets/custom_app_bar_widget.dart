186111e2305324067d9e09651a0d92412c9a2c10
Tolga Yildirim <tolgayld1990@gmail.com>
2024-02-18 20:54:02 +0100

    Stateful Widget converted to Stateless Widget

--- lib/src/presentation/widgets/custom_app_bar_widget.dart
+++ lib/src/presentation/widgets/custom_app_bar_widget.dart
@@ -3,17 +3,13 @@ import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:locall_app/core/common/gen/assets.gen.dart';
 
-class CustomAppBarWidget extends StatefulWidget implements PreferredSizeWidget {
+class CustomAppBarWidget extends StatelessWidget
+    implements PreferredSizeWidget {
   const CustomAppBarWidget({super.key});
 
   @override
-  State<CustomAppBarWidget> createState() => _CustomAppBarWidgetState();
-
-  @override
   Size get preferredSize => const Size.fromHeight(100);
-}
 
-class _CustomAppBarWidgetState extends State<CustomAppBarWidget> {
   @override
   Widget build(BuildContext context) {
     return PlatformAppBar(
