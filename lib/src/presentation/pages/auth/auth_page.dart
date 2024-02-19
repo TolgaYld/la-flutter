bb30e275c658a3cb5f5dbf400921e7d30790ef6f
Tolga Yildirim <tolgayld1990@gmail.com>
2024-02-19 20:53:39 +0100

    AppBar added

--- lib/src/presentation/pages/auth/auth_page.dart
+++ lib/src/presentation/pages/auth/auth_page.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:locall_app/src/presentation/widgets/custom_app_bar_widget.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:locall_app/core/common/gen/assets.gen.dart';
+import 'package:locall_app/src/presentation/widgets/custom_body_widget.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBarWidget(),
+    return PlatformScaffold(
+      appBar: PlatformAppBar(
+        title: SvgPicture.asset(Assets.images.logoipsum285)
+            .animate()
+            .fade(duration: 210.ms)
+            .then()
+            .shimmer(duration: 210.ms),
+      ),
+      body: const CustomBodyWidget(),
     );
   }
 }
