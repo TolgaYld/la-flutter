86281ced759849dc4131b0f9eb29cc10312b19a0
Tolga Yildirim <tolgayld1990@gmail.com>
2024-02-19 20:54:17 +0100

    widgets for auth

---
+++ lib/src/presentation/widgets/custom_body_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomBodyWidget extends StatelessWidget {
+  const CustomBodyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/src/presentation/widgets/custom_text_form_field_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CustomTextEdittingController extends StatefulWidget {
+  const CustomTextEdittingController({super.key});
+
+  @override
+  State<CustomTextEdittingController> createState() =>
+      _CustomTextEdittingControllerState();
+}
+
+class _CustomTextEdittingControllerState
+    extends State<CustomTextEdittingController> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
