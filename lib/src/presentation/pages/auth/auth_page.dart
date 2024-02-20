4bbcf20d15d13bf0db0dcc6e65d0a0838b681d13
Tolga Yildirim <tolgayld1990@gmail.com>
2024-02-20 21:38:34 +0100

    Changed from PlattforScaffold to Scaffold

--- lib/src/presentation/pages/auth/auth_page.dart
+++ lib/src/presentation/pages/auth/auth_page.dart
@@ -1,24 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
-import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:locall_app/core/common/gen/assets.gen.dart';
-import 'package:locall_app/src/presentation/widgets/custom_body_widget.dart';
+import 'package:locall_app/src/presentation/widgets/auth/custom_body_widget.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return PlatformScaffold(
-      appBar: PlatformAppBar(
+    return Scaffold(
+      appBar: AppBar(
         title: SvgPicture.asset(Assets.images.logoipsum285)
             .animate()
             .fade(duration: 210.ms)
             .then()
             .shimmer(duration: 210.ms),
       ),
-      body: const CustomBodyWidget(),
+      body: CustomBodyWidget(),
     );
   }
 }
