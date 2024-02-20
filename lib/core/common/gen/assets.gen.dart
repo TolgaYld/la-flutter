c1bb29d6ab4bf0d6f2249ced47100476e89c4153
Tolga Yildirim <tolgayld1990@gmail.com>
2024-02-20 21:36:58 +0100

    generated file

--- lib/core/common/gen/assets.gen.dart
+++ lib/core/common/gen/assets.gen.dart
@@ -7,12 +7,6 @@
 // ignore_for_file: type=lint
 // ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
 
-class $LibGen {
-  const $LibGen();
-
-  $LibCoreGen get core => const $LibCoreGen();
-}
-
 class $AssetsImagesGen {
   const $AssetsImagesGen();
 
@@ -23,31 +17,8 @@ class $AssetsImagesGen {
   List<String> get values => [logoipsum285];
 }
 
-class $LibCoreGen {
-  const $LibCoreGen();
-
-  $LibCoreCommonGen get common => const $LibCoreCommonGen();
-}
-
-class $LibCoreCommonGen {
-  const $LibCoreCommonGen();
-
-  $LibCoreCommonEnvGen get env => const $LibCoreCommonEnvGen();
-}
-
-class $LibCoreCommonEnvGen {
-  const $LibCoreCommonEnvGen();
-
-  /// File path: lib/core/common/env/.env.development
-  String get env => 'lib/core/common/env/.env.development';
-
-  /// List of all assets
-  List<String> get values => [env];
-}
-
 class Assets {
   Assets._();
 
   static const $AssetsImagesGen images = $AssetsImagesGen();
-  static const $LibGen lib = $LibGen();
 }
