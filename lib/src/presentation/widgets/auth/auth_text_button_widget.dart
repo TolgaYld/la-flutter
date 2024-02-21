4796a260dcd9e812f7106ad8aaef65931064a73b
Tolga Yildirim <tolgayld1990@gmail.com>
2024-02-21 21:20:54 +0100

    new possibilities added to customize widget

--- lib/src/presentation/widgets/auth/auth_text_button_widget.dart
+++ lib/src/presentation/widgets/auth/auth_text_button_widget.dart
@@ -5,23 +5,27 @@ class AuthTextButtonWidget extends StatelessWidget {
   const AuthTextButtonWidget({
     required this.text,
     required this.onPressed,
+    this.differentTextStyle,
+    this.wantPadding = true,
     super.key,
   });
 
   final String text;
   final VoidCallback onPressed;
+  final bool wantPadding;
+  final TextStyle? differentTextStyle;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(
-        right: context.paddingVerticalForTextField,
+        right: wantPadding ? context.paddingVerticalWidgets : 0,
       ),
       child: TextButton(
         onPressed: onPressed,
         child: Text(
           text,
-          style: context.theme.textTheme.labelSmall,
+          style: differentTextStyle ?? context.theme.textTheme.labelSmall,
         ),
       ),
     );
