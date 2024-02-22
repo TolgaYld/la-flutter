4a74211e8dc8805f55a14856dabd0453254a4bc1
Tolga Yildirim <tolgayld1990@gmail.com>
2024-02-22 17:35:27 +0100

    class name changed (L10 to LanguageManager)

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,8 @@ import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hive/hive.dart';
-import 'package:locall_app/core/common/l10n/l10n.dart';
 import 'package:locall_app/core/services/dependency_injection/injection.dart';
+import 'package:locall_app/core/utils/l10n/language_manager.dart';
 import 'package:locall_app/src/application/theme_mode/theme_mode.dart';
 import 'package:locall_app/src/presentation/pages/auth/auth_page.dart';
 import 'package:locall_app/theme.dart';
@@ -39,13 +39,13 @@ class MyApp extends StatelessWidget {
         builder: (context, themeService, child) {
           return MaterialApp(
             debugShowCheckedModeBanner: false,
-            supportedLocales: L10n.all,
             localizationsDelegates: const [
               AppLocalizations.delegate,
               GlobalMaterialLocalizations.delegate,
               GlobalCupertinoLocalizations.delegate,
               GlobalWidgetsLocalizations.delegate,
             ],
+            supportedLocales: LanguageManager.supportedLocales,
             title: 'La-Flutter',
             theme: AppTheme.lightTheme,
             themeMode: themeService.useSystemTheme
