cf2049fdaf1311e2eee66bfbd0d7e41a50b4f63b
Tolga Yildirim <tolgayld1990@gmail.com>
2024-02-22 17:35:37 +0100

    generated file

--- lib/core/common/gen/assets.gen.dart
+++ lib/core/common/gen/assets.gen.dart
@@ -17,8 +17,28 @@ class $AssetsImagesGen {
   List<String> get values => [logoipsum285];
 }
 
+class $AssetsTranslationsGen {
+  const $AssetsTranslationsGen();
+
+  /// File path: assets/translations/json_de.arb
+  String get jsonDe => 'assets/translations/json_de.arb';
+
+  /// File path: assets/translations/json_en.arb
+  String get jsonEn => 'assets/translations/json_en.arb';
+
+  /// File path: assets/translations/json_es.arb
+  String get jsonEs => 'assets/translations/json_es.arb';
+
+  /// File path: assets/translations/json_tr.arb
+  String get jsonTr => 'assets/translations/json_tr.arb';
+
+  /// List of all assets
+  List<String> get values => [jsonDe, jsonEn, jsonEs, jsonTr];
+}
+
 class Assets {
   Assets._();
 
   static const $AssetsImagesGen images = $AssetsImagesGen();
+  static const $AssetsTranslationsGen translations = $AssetsTranslationsGen();
 }
