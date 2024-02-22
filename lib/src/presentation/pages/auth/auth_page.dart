45b52c56880284ea643bf7da8751c9161a5085ae
Tolga Yildirim <tolgayld1990@gmail.com>
2024-02-22 17:37:56 +0100

    MultiBlocProvider added

--- lib/src/presentation/pages/auth/auth_page.dart
+++ lib/src/presentation/pages/auth/auth_page.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:locall_app/core/common/gen/assets.gen.dart';
+import 'package:locall_app/core/services/dependency_injection/injection.dart';
+import 'package:locall_app/src/application/auth/bloc/auth_bloc.dart';
+import 'package:locall_app/src/application/auth/cubit/auth_mode_cubit.dart';
+import 'package:locall_app/src/application/auth/cubit/check_if_taken_cubit.dart';
 import 'package:locall_app/src/presentation/widgets/auth/auth_body_widget.dart';
 
 class AuthPage extends StatelessWidget {
@@ -17,7 +22,18 @@ class AuthPage extends StatelessWidget {
             .then()
             .shimmer(duration: 210.ms),
       ),
-      body: AuthBodyWidget(),
+      body: MultiBlocProvider(
+        providers: [
+          BlocProvider<AuthModeCubit>(
+            create: (context) => sl<AuthModeCubit>(),
+          ),
+          BlocProvider<CheckIfTakenCubit>(
+            create: (context) => sl<CheckIfTakenCubit>(),
+          ),
+          BlocProvider<AuthBloc>(create: (context) => sl<AuthBloc>()),
+        ],
+        child: AuthBodyWidget(),
+      ),
     );
   }
 }
