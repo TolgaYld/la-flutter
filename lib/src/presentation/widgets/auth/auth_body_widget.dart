1ec35125a3c695bc78db16f1d530fde5f206dd5d
Tolga Yildirim <tolgayld1990@gmail.com>
2024-02-27 00:51:41 +0100

    blocconsumer and new widgets added

--- lib/src/presentation/widgets/auth/auth_body_widget.dart
+++ lib/src/presentation/widgets/auth/auth_body_widget.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:locall_app/core/extensions/context_extension.dart';
+import 'package:locall_app/src/application/auth/bloc/auth_bloc.dart';
 import 'package:locall_app/src/application/auth/cubit/auth_mode_cubit.dart';
 import 'package:locall_app/src/presentation/widgets/auth/auth_text_button_widget.dart';
 import 'package:locall_app/src/presentation/widgets/auth/form_builder_forgot_password_widget.dart';
@@ -24,74 +25,108 @@ class AuthBodyWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final notAUserString = AppLocalizations.of(context)!.not_a_user_yet;
     final alreadyAUserString = AppLocalizations.of(context)!.already_a_user_yet;
-    final registerString = AppLocalizations.of(context)!.register;
+    final signUpString = AppLocalizations.of(context)!.sign_up;
     final backToLoginString = AppLocalizations.of(context)!.back_to_login;
-    return BlocBuilder<AuthModeCubit, AuthModeState>(
-      builder: (context, state) {
-        return Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              child: state.when(
-                signIn: () => FormBuilderSignInWidget(
-                  key: signInKey,
-                  signInFormKey: _signInFormKey,
+    final signInString = AppLocalizations.of(context)!.sign_in;
+    final resetPasswordString = AppLocalizations.of(context)!.reset_password;
+    return BlocConsumer<AuthBloc, AuthState>(
+      listener: (context, state) {
+        // TODO: implement listener
+      },
+      builder: (context, authState) {
+        return BlocBuilder<AuthModeCubit, AuthModeState>(
+          builder: (context, modeState) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: context.paddingHorizontalWidgets * 6,
+                  ),
+                  child: modeState.when(
+                    signIn: () => FormBuilderSignInWidget(
+                      key: signInKey,
+                      signInFormKey: _signInFormKey,
+                    ),
+                    signUp: () => FormBuilderSignUpWidget(
+                      key: signUpKey,
+                      signUpFormKey: _signUpFormKey,
+                    ),
+                    forgotPassword: () => FormBuilderForgotPasswordWidget(
+                      key: forgotPasswordKey,
+                      forgotPasswordFormKey: _forgotPasswordFormKey,
+                    ),
+                  ),
                 ),
-                signUp: () => FormBuilderSignUpWidget(
-                  key: signUpKey,
-                  signUpFormKey: _signUpFormKey,
-                ),
-                forgotPassword: () => FormBuilderForgotPasswordWidget(
-                  key: forgotPasswordKey,
-                  forgotPasswordFormKey: _forgotPasswordFormKey,
-                ),
-              ),
-            ),
-            if (state == const AuthModeState.signIn() ||
-                state == const AuthModeState.signUp())
-              Padding(
-                padding: EdgeInsets.only(
-                  bottom: context.paddingVerticalWidgets * 3,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    state.maybeWhen(
-                      orElse: SizedBox.new,
-                      signIn: () => Text(
-                        notAUserString,
-                        style: context.theme.textTheme.labelSmall,
-                      ),
-                      signUp: () => Text(
-                        alreadyAUserString,
-                        style: context.theme.textTheme.labelSmall,
+                Padding(
+                  padding: EdgeInsets.only(
+                    bottom: modeState == const AuthModeState.forgotPassword()
+                        ? context.paddingHorizontalWidgets * 15
+                        : 0,
+                  ),
+                  child: authState.maybeWhen(
+                    orElse: () => ElevatedButton(
+                      onPressed: () {},
+                      child: modeState.when(
+                        signIn: () => Text(signInString),
+                        signUp: () => Text(signUpString),
+                        forgotPassword: () => Text(resetPasswordString),
                       ),
                     ),
-                    state.maybeWhen(
-                      orElse: SizedBox.new,
-                      signIn: () => AuthTextButtonWidget(
-                        text: registerString,
-                        onPressed: () => context.read<AuthModeCubit>().signUp(),
-                        wantPadding: false,
-                        differentTextStyle:
-                            context.theme.textTheme.labelSmall!.copyWith(
-                          color: context.theme.colorScheme.secondary,
-                        ),
-                      ),
-                      signUp: () => AuthTextButtonWidget(
-                        text: backToLoginString,
-                        onPressed: () => context.read<AuthModeCubit>().signIn(),
-                        wantPadding: false,
-                        differentTextStyle:
-                            context.theme.textTheme.labelSmall!.copyWith(
-                          color: context.theme.colorScheme.secondary,
-                        ),
-                      ),
+                    loading: () => SpinKitWave(
+                      color: context.theme.colorScheme.onPrimary,
                     ),
-                  ],
+                  ),
                 ),
-              ),
-          ],
+                if (modeState == const AuthModeState.signIn() ||
+                    modeState == const AuthModeState.signUp())
+                  Padding(
+                    padding: EdgeInsets.only(
+                      bottom: context.paddingHorizontalWidgets,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        modeState.maybeWhen(
+                          orElse: SizedBox.new,
+                          signIn: () => Text(
+                            notAUserString,
+                            style: context.theme.textTheme.labelSmall,
+                          ),
+                          signUp: () => Text(
+                            alreadyAUserString,
+                            style: context.theme.textTheme.labelSmall,
+                          ),
+                        ),
+                        modeState.maybeWhen(
+                          orElse: SizedBox.new,
+                          signIn: () => AuthTextButtonWidget(
+                            text: signUpString,
+                            onPressed: () =>
+                                context.read<AuthModeCubit>().signUp(),
+                            wantPadding: false,
+                            differentTextStyle:
+                                context.theme.textTheme.labelSmall!.copyWith(
+                              color: context.theme.colorScheme.secondary,
+                            ),
+                          ),
+                          signUp: () => AuthTextButtonWidget(
+                            text: backToLoginString,
+                            onPressed: () =>
+                                context.read<AuthModeCubit>().signIn(),
+                            wantPadding: false,
+                            differentTextStyle:
+                                context.theme.textTheme.labelSmall!.copyWith(
+                              color: context.theme.colorScheme.secondary,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+              ],
+            );
+          },
         );
       },
     );
