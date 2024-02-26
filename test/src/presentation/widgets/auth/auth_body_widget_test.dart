d266966c32aea8f5efb837d3d7235a6c95773d07
Tolga Yildirim <tolgayld1990@gmail.com>
2024-02-27 00:52:29 +0100

    authbloc widget testing...

--- test/src/presentation/widgets/auth/auth_body_widget_test.dart
+++ test/src/presentation/widgets/auth/auth_body_widget_test.dart
@@ -1,29 +1,54 @@
 import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:locall_app/src/application/auth/bloc/auth_bloc.dart';
 import 'package:locall_app/src/application/auth/cubit/auth_mode_cubit.dart';
 import 'package:locall_app/src/presentation/widgets/auth/auth_body_widget.dart';
+// ignore: depend_on_referenced_packages
+import 'package:mocktail/mocktail.dart';
 
 import '../../test_scaffold_wrapper.dart';
 
 class MockAuthModeCubit extends MockCubit<AuthModeState>
     implements AuthModeCubit {}
 
+class MockAuthBloc extends MockBloc<AuthEvent, AuthState> implements AuthBloc {}
+
+class FakeAuthEvent extends Fake implements AuthEvent {}
+
+class FakeAuthState extends Fake implements AuthState {}
+
 void main() async {
   late MockAuthModeCubit authModeCubit;
+  late MockAuthBloc authBloc;
   group('AuthBodyWidget', () {
     final findSignInWidget = find.byKey(AuthBodyWidget.signInKey);
     final findSignUpWidget = find.byKey(AuthBodyWidget.signUpKey);
     final findForgotPasswordWidget =
         find.byKey(AuthBodyWidget.forgotPasswordKey);
+    final findLoadingIndicator = find.byType(SpinKitWave);
+    final findElevatedButton = find.byType(ElevatedButton);
+
+    setUpAll(() {
+      registerFallbackValue(FakeAuthEvent());
+      registerFallbackValue(FakeAuthState());
+    });
+
     setUp(() {
       authModeCubit = MockAuthModeCubit();
+      authBloc = MockAuthBloc();
     });
-    tearDown(() => authModeCubit.close());
+    tearDown(() {
+      authModeCubit.close();
+      authBloc.close();
+    });
 
     testWidgets(
       'body should display [FormBuilderSignInWidget]',
       (tester) async {
+        when(() => authBloc.state).thenReturn(const AuthState.initial());
         whenListen(
           authModeCubit,
           Stream.fromIterable([const AuthModeState.signIn()]),
@@ -32,8 +57,15 @@ void main() async {
 
         await tester.pumpWidget(
           TestScaffoldWrapper(
-            child: BlocProvider<AuthModeCubit>(
-              create: (context) => authModeCubit,
+            child: MultiBlocProvider(
+              providers: [
+                BlocProvider<AuthModeCubit>(
+                  create: (context) => authModeCubit,
+                ),
+                BlocProvider<AuthBloc>(
+                  create: (context) => authBloc,
+                ),
+              ],
               child: AuthBodyWidget(),
             ),
           ),
@@ -50,6 +82,7 @@ void main() async {
     testWidgets(
       'body should display [FormBuilderSignUpWidget]',
       (tester) async {
+        when(() => authBloc.state).thenReturn(const AuthState.initial());
         whenListen(
           authModeCubit,
           Stream.fromIterable([const AuthModeState.signUp()]),
@@ -58,8 +91,15 @@ void main() async {
 
         await tester.pumpWidget(
           TestScaffoldWrapper(
-            child: BlocProvider<AuthModeCubit>(
-              create: (context) => authModeCubit,
+            child: MultiBlocProvider(
+              providers: [
+                BlocProvider<AuthModeCubit>(
+                  create: (context) => authModeCubit,
+                ),
+                BlocProvider<AuthBloc>(
+                  create: (context) => authBloc,
+                ),
+              ],
               child: AuthBodyWidget(),
             ),
           ),
@@ -76,6 +116,7 @@ void main() async {
     testWidgets(
       'body should display [FormBuilderForgotPasswordWidget]',
       (tester) async {
+        when(() => authBloc.state).thenReturn(const AuthState.initial());
         whenListen(
           authModeCubit,
           Stream.fromIterable([const AuthModeState.forgotPassword()]),
@@ -84,8 +125,15 @@ void main() async {
 
         await tester.pumpWidget(
           TestScaffoldWrapper(
-            child: BlocProvider<AuthModeCubit>(
-              create: (context) => authModeCubit,
+            child: MultiBlocProvider(
+              providers: [
+                BlocProvider<AuthModeCubit>(
+                  create: (context) => authModeCubit,
+                ),
+                BlocProvider<AuthBloc>(
+                  create: (context) => authBloc,
+                ),
+              ],
               child: AuthBodyWidget(),
             ),
           ),
@@ -96,6 +144,34 @@ void main() async {
         expect(findSignInWidget, findsNothing);
         expect(findForgotPasswordWidget, findsOneWidget);
         expect(findSignUpWidget, findsNothing);
+      },
+    );
+
+    testWidgets(
+      'body should display [CircularProgressIndicator]',
+      (tester) async {
+        when(() => authModeCubit.state)
+            .thenReturn(const AuthModeState.signIn());
+        when(() => authBloc.state).thenReturn(const AuthState.loading());
+
+        await tester.pumpWidget(
+          TestScaffoldWrapper(
+            child: MultiBlocProvider(
+              providers: [
+                BlocProvider<AuthModeCubit>(
+                  create: (context) => authModeCubit,
+                ),
+                BlocProvider<AuthBloc>(
+                  create: (context) => authBloc,
+                ),
+              ],
+              child: AuthBodyWidget(),
+            ),
+          ),
+        );
+
+        expect(findLoadingIndicator, findsOneWidget);
+        expect(findElevatedButton, findsNothing);
       },
     );
   });
