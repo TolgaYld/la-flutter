8d1d51bdf343ae735d4f700fea0d80409f98cf98
Tolga Yildirim <tolgayld1990@gmail.com>
2024-02-28 13:36:56 +0100

    enum directyl imported from geolocator libary

--- lib/src/domain/repositories/gps_status/gps_status_repo.dart
+++ lib/src/domain/repositories/gps_status/gps_status_repo.dart
@@ -1,7 +1,7 @@
+import 'package:geolocator/geolocator.dart';
 import 'package:locall_app/core/utils/typedefs.dart';
-import 'package:locall_app/src/domain/entities/gps_status.dart';
 
 // ignore: one_member_abstracts
 abstract class GpsStatusRepo {
-  ResultStream<GpsStatus> watchGpsStatus();
+  ResultStream<ServiceStatus> watchGpsStatus();
 }
