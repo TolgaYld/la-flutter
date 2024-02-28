a877fad48af41e4d9f51d4e8fb7333ec17b6dd52
Tolga Yildirim <tolgayld1990@gmail.com>
2024-02-28 19:40:18 +0100

    service locator added for watching locationservicestatus

---
+++ lib/core/services/dependency_injection/gps_status/injection.dart
@@ -0,0 +1,27 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:get_it/get_it.dart';
+import 'package:locall_app/src/application/gps_status/cubit/gps_status_cubit.dart';
+import 'package:locall_app/src/data/datasources/gps_status/gps_status_local_datasrc.dart';
+import 'package:locall_app/src/data/repositories/gps_status/gps_status_repo_impl.dart';
+import 'package:locall_app/src/domain/repositories/gps_status/gps_status_repo.dart';
+import 'package:locall_app/src/domain/usecases/gps_status/watch_gps_status_usecase.dart';
+
+Future<void> initGpsStatus(GetIt sl) async {
+  // Cubit - App Logic
+  sl
+    ..registerFactory(() => GpsStatusCubit(sl()))
+
+    // Usecases
+    ..registerLazySingleton(() => WatchGpsStatusUsecase(sl()))
+
+    // Repo
+    ..registerLazySingleton<GpsStatusRepo>(() => GpsStatusRepoImpl(sl()))
+
+    // Datasrc
+    ..registerLazySingleton<GpsStatusLocalDatasrc>(
+      () => GpsStatusLocalDatasrcImpl(sl()),
+    )
+
+    // External
+    ..registerLazySingleton(() => GeolocatorPlatform.instance);
+}
--- lib/core/services/dependency_injection/injection.main.dart
+++ lib/core/services/dependency_injection/injection.main.dart
@@ -3,6 +3,7 @@ part of 'injection.dart';
 final sl = GetIt.instance;
 
 Future<void> init() async {
+  await initGpsStatus(sl);
   await initAuth(sl);
   await initThemeMode(sl);
 }
