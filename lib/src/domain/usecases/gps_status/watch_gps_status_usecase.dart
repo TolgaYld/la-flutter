2858862a4632a04a4b50a479ea71fee1c2ec84d8
Tolga Yildirim <tolgayld1990@gmail.com>
2024-02-28 13:37:04 +0100

    enum directyl imported from geolocator libary

--- lib/src/domain/usecases/gps_status/watch_gps_status_usecase.dart
+++ lib/src/domain/usecases/gps_status/watch_gps_status_usecase.dart
@@ -1,12 +1,12 @@
+import 'package:geolocator/geolocator.dart';
 import 'package:locall_app/core/usecases/usecases.dart';
 import 'package:locall_app/core/utils/typedefs.dart';
-import 'package:locall_app/src/domain/entities/gps_status.dart';
 import 'package:locall_app/src/domain/repositories/gps_status/gps_status_repo.dart';
 
-class WatchGpsStatusUsecase extends UsecaseWithoutParams<GpsStatus> {
+class WatchGpsStatusUsecase extends UsecaseWithoutParams<ServiceStatus> {
   const WatchGpsStatusUsecase(this._repo);
 
   final GpsStatusRepo _repo;
   @override
-  ResultStream<GpsStatus> call() => _repo.watchGpsStatus();
+  ResultStream<ServiceStatus> call() => _repo.watchGpsStatus();
 }
