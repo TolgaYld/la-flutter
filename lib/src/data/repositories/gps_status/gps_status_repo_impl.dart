ce7d78456eda8a2ffec5d3d7b2cb53d8b0928b33
Tolga Yildirim <tolgayld1990@gmail.com>
2024-02-28 13:36:12 +0100

    implementation of interface

---
+++ lib/src/data/repositories/gps_status/gps_status_repo_impl.dart
@@ -0,0 +1,27 @@
+import 'package:dartz/dartz.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:locall_app/core/errors/exceptions.dart';
+import 'package:locall_app/core/errors/failures.dart';
+import 'package:locall_app/core/utils/typedefs.dart';
+import 'package:locall_app/src/data/datasources/gps_status/gps_status_local_datasrc.dart';
+import 'package:locall_app/src/domain/repositories/gps_status/gps_status_repo.dart';
+import 'package:rxdart/rxdart.dart';
+
+class GpsStatusRepoImpl implements GpsStatusRepo {
+  const GpsStatusRepoImpl(this._datasrc);
+
+  final GpsStatusLocalDatasrc _datasrc;
+  @override
+  ResultStream<ServiceStatus> watchGpsStatus() {
+    return _datasrc
+        .getServiceStatusStream()
+        .map<Either<Failure, ServiceStatus>>(Right.new)
+        .onErrorReturnWith(
+          (error, stackTrace) => Left(
+            GpsStatusFailure.fromException(
+              GpsStatusException(message: error.toString()),
+            ),
+          ),
+        );
+  }
+}
