6751a2b45d1cb8d7891e21c694af5a8c7c765385
Tolga Yildirim <tolgayld1990@gmail.com>
2024-02-28 19:38:53 +0100

    blocprovider and builder added, to switch page if locationservice is disabled

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:locall_app/core/services/dependency_injection/injection.dart';
 import 'package:locall_app/core/utils/l10n/language_manager.dart';
+import 'package:locall_app/src/application/gps_status/cubit/gps_status_cubit.dart';
 import 'package:locall_app/src/application/theme_mode/theme_mode.dart';
 import 'package:locall_app/src/presentation/pages/auth/auth_page.dart';
 import 'package:locall_app/theme.dart';
@@ -25,33 +28,38 @@ class MyApp extends StatelessWidget {
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
     ]);
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider.value(
-          value: sl<ThemeModeServiceProvider>(),
+    return BlocProvider<GpsStatusCubit>(
+      create: (context) => sl<GpsStatusCubit>()..watchLocationStatus(),
+      child: ChangeNotifierProvider.value(
+        value: sl<ThemeModeServiceProvider>(),
+        child: BlocBuilder<GpsStatusCubit, GpsStatusState>(
+          builder: (context, state) {
+            return Consumer<ThemeModeServiceProvider>(
+              builder: (context, themeService, child) {
+                return MaterialApp(
+                  debugShowCheckedModeBanner: false,
+                  localizationsDelegates: const [
+                    AppLocalizations.delegate,
+                    GlobalMaterialLocalizations.delegate,
+                    GlobalCupertinoLocalizations.delegate,
+                    GlobalWidgetsLocalizations.delegate,
+                  ],
+                  supportedLocales: LanguageManager.supportedLocales,
+                  title: 'La-Flutter',
+                  theme: AppTheme.lightTheme,
+                  themeMode: themeService.useSystemTheme
+                      ? ThemeMode.system
+                      : themeService.isDarkModeOn
+                          ? ThemeMode.dark
+                          : ThemeMode.light,
+                  home: state.status == ServiceStatus.enabled
+                      ? const AuthPage()
+                      : Container(),
+                );
+              },
+            );
+          },
         ),
-      ],
-      child: Consumer<ThemeModeServiceProvider>(
-        builder: (context, themeService, child) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            localizationsDelegates: const [
-              AppLocalizations.delegate,
-              GlobalMaterialLocalizations.delegate,
-              GlobalCupertinoLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate,
-            ],
-            supportedLocales: LanguageManager.supportedLocales,
-            title: 'La-Flutter',
-            theme: AppTheme.lightTheme,
-            themeMode: themeService.useSystemTheme
-                ? ThemeMode.system
-                : themeService.isDarkModeOn
-                    ? ThemeMode.dark
-                    : ThemeMode.light,
-            home: const AuthPage(),
-          );
-        },
       ),
     );
   }
