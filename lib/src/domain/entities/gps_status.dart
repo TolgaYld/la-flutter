54899e251e40771a35d387a81554ffe8921f5933
Tolga Yildirim <tolgayld1990@gmail.com>
2024-02-28 13:36:29 +0100

    unnecessary

--- lib/src/domain/entities/gps_status.dart
+++
@@ -1,8 +0,0 @@
-import 'package:locall_app/core/types/location_service_status.dart';
-
-class GpsStatus {
-  const GpsStatus(this.status);
-
-  factory GpsStatus.empty() => const GpsStatus(LocationServiceStatus.enabled);
-  final LocationServiceStatus status;
-}
